88386b11ec061df9bb4650be0bea84e652f6cdea
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-02-04 00:36:47 +0200

    change the title

--- sample_project/lib/main.dart
+++ sample_project/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Sample Project',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
