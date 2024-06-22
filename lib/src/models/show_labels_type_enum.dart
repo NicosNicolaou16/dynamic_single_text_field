9557ba9208e786b0363f34995628cb426b1ed9f8
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-06-22 22:40:52 +0300

    add some documentation

--- lib/src/models/show_labels_type_enum.dart
+++ lib/src/models/show_labels_type_enum.dart
@@ -1,6 +1,14 @@
+/// This enum class is using to show the label text
 enum ShowLabelsTypeEnum {
+  /// Show only the top text label on single text
   showTopLabelType,
+
+  /// Show only the bottom text label on single text
   showBottomLabelType,
+
+  /// Show both top and bottom text label on single text
   showBothLabelsType,
+
+  /// Disable both text label, this is the default value
   hideLabelsType
 }
--- lib/src/models/single_text_model.dart
+++ lib/src/models/single_text_model.dart
@@ -1,6 +1,12 @@
+/// This model is using to create the number of the single text boxes
 class SingleTextModel {
+  /// Text to show - prefilled
   String singleText;
+
+  /// Top label text on top of the single text box
   String? topLabelText;
+
+  /// Bottom label text on top of the single text box
   String? bottomLabelText;
 
   SingleTextModel({
